a323b7fb4483ebbaa1e85ba6ff600482c1d609e8
yojoshb <68751897+yojoshb@users.noreply.github.com>
2021-07-20 21:31:44 -0500

    Update script for Veeam B&R 11
    Veeam PowerShell Snapin has been depreciated.
    Add module for local and remote vbr host, fix the repository info function to fetch data correctly for local and remote vbr host, add totalbackuptransfer output

--- veeam-stats.ps1
+++ veeam-stats.ps1
@@ -36,55 +36,128 @@ param(
 # Big thanks to Shawn, creating a awsome Reporting Script:
 # http://blog.smasterson.com/2016/02/16/veeam-v9-my-veeam-report-v9-0-1/
 
-#region: Start Load VEEAM Snapin (if not already loaded)
-if (!(Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue)) {
-	if (!(Add-PSSnapin -PassThru VeeamPSSnapIn)) {
-		# Error out if loading fails
-		Write-Error "`nERROR: Cannot load the VEEAM Snapin."
-		Exit
-	}
+# Veaam Snapin is depreciated!
+#region: Start Load VEEAM Snapin / Module (in local or remote session)
+if ($PSRemote) {
+    # Remoting on VBR server
+    $RemoteSession = New-PSSession -Authentication Kerberos -ComputerName $BRHost
+    if (-not $RemoteSession){throw "Cannot open remote session on '$BRHost' with user '$env:USERNAME'"}
+
+    # Loading Module or PSSnapin then retrieve commands
+    Invoke-Command -Session $RemoteSession -ScriptBlock {
+        # Make sure PSModulePath includes Veeam Console
+        $MyModulePath = "C:\Program Files\Veeam\Backup and Replication\Console\"
+        $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$MyModulePath"
+        if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
+        try {
+            $Modules | Import-Module -WarningAction SilentlyContinue
+            }
+            catch {
+                throw "Failed to load Veeam Modules"
+                }
+        }
+        else {
+            Write-Host "No Veeam Modules found, Fallback to SnapIn."
+            try {
+                Add-PSSnapin -PassThru VeeamPSSnapIn -ErrorAction Stop | Out-Null
+                }
+                catch {
+                    throw "Failed to load VeeamPSSnapIn and no Modules found"
+                    }
+        }
+    } -ErrorAction Stop
+    Import-PSSession -Session $RemoteSession -Module VeeamPSSnapin -ErrorAction Stop | Out-Null
+} else {
+    # Loading Module or PSSnapin
+    # Make sure PSModulePath includes Veeam Console
+    $MyModulePath = "C:\Program Files\Veeam\Backup and Replication\Console\"
+    $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$MyModulePath"
+    if ($Modules = Get-Module -ListAvailable -Name Veeam.Backup.PowerShell) {
+        try {
+            $Modules | Import-Module -WarningAction SilentlyContinue
+            }
+            catch {
+                throw "Failed to load Veeam Modules"
+                }
+        }
+        else {
+            Write-Host "No Veeam Modules found, Fallback to SnapIn."
+            try {
+                Add-PSSnapin -PassThru VeeamPSSnapIn -ErrorAction Stop | Out-Null
+                }
+                catch {
+                    throw "Failed to load VeeamPSSnapIn and no Modules found"
+                    }
+        }
 }
 #endregion
 
 #region: Functions
 Function Get-vPCRepoInfo {
 [CmdletBinding()]
-        param (
-                [Parameter(Position=0, ValueFromPipeline=$true)]
-                [PSObject[]]$Repository
-                )
-        Begin {
-                $outputAry = @()
-                Function Build-Object {param($name, $repohost, $path, $free, $total)
-                        $repoObj = New-Object -TypeName PSObject -Property @{
-                                        Target = $name
-										RepoHost = $repohost
-                                        Storepath = $path
-                                        StorageFree = [Math]::Round([Decimal]$free/1GB,2)
-                                        StorageTotal = [Math]::Round([Decimal]$total/1GB,2)
-                                        FreePercentage = [Math]::Round(($free/$total)*100)
-                                }
-                        Return $repoObj | Select Target, RepoHost, Storepath, StorageFree, StorageTotal, FreePercentage
+    param (
+        [Parameter(Position=0, ValueFromPipeline=$true)]
+        [PSObject[]]$Repository
+    )
+    Begin {
+        $outputAry = @()
+        Function New-RepoObject {param($name, $repohost, $path, $free, $total)
+        $repoObj = New-Object -TypeName PSObject -Property @{
+            Target = $name
+            RepoHost = $repohost
+                        Storepath = $path
+                        StorageFree = [Math]::Round([Decimal]$free/1GB,2)
+                        StorageTotal = [Math]::Round([Decimal]$total/1GB,2)
+                        FreePercentage = [Math]::Round(($free/$total)*100)
+            }
+        Return $repoObj | Select-Object Target, RepoHost, Storepath, StorageFree, StorageTotal, FreePercentage
+        }
+    }
+    Process {
+        Foreach ($r in $Repository) {
+            # Refresh Repository Size Info
+            try {
+                if ($PSRemote) {
+                    $SyncSpaceCode = {
+                        param($RepositoryName);
+                        [Veeam.Backup.Core.CBackupRepositoryEx]::SyncSpaceInfoToDb((Get-VBRBackupRepository -Name $RepositoryName), $true)
+                    }
+
+                    Invoke-Command -Session $RemoteSession -ScriptBlock $SyncSpaceCode -ArgumentList $r.Name
+                } else {
+                    [Veeam.Backup.Core.CBackupRepositoryEx]::SyncSpaceInfoToDb($r, $true)
                 }
+
+            }
+            catch {
+                Write-Debug "SyncSpaceInfoToDb Failed"
+                Write-Error $_.ToString()
+                Write-Error $_.ScriptStackTrace
+            }
+            If ($r.HostId -eq "00000000-0000-0000-0000-000000000000") {
+                $HostName = ""
+            }
+            Else {
+                $HostName = $(Get-VBRServer | Where-Object {$_.Id -eq $r.HostId}).Name.ToLower()
+            }
+
+            if ($PSRemote) {
+            # When veeam commands are invoked remotly they are serialized during transfer. The info property become not object but string.
+            # To gather the info following construction should be used
+                $r.info = Invoke-Command -Session $RemoteSession -HideComputerName -ScriptBlock {
+                    param($RepositoryName);
+                    (Get-VBRBackupRepository -Name $RepositoryName).info
+                } -ArgumentList $r.Name
+            }
+
+            Write-Debug $r.Info
+            $outputObj = New-RepoObject $r.Name $Hostname $r.FriendlyPath $r.GetContainer().CachedFreeSpace.InBytes $r.GetContainer().CachedTotalSpace.InBytes
         }
-        Process {
-                Foreach ($r in $Repository) {
-                	# Refresh Repository Size Info
-					[Veeam.Backup.Core.CBackupRepositoryEx]::SyncSpaceInfoToDb($r, $true)
-					
-					If ($r.HostId -eq "00000000-0000-0000-0000-000000000000") {
-						$HostName = ""
-					}
-					Else {
-						$HostName = $($r.GetHost()).Name.ToLower()
-					}
-					$outputObj = Build-Object $r.Name $Hostname $r.Path $r.info.CachedFreeSpace $r.Info.CachedTotalSpace
-					}
-                $outputAry += $outputObj
-        }
-        End {
-                $outputAry
-        }
+        $outputAry += $outputObj
+    }
+    End {
+        $outputAry
+    }
 }
 #endregion
 
@@ -252,6 +325,7 @@ $body="veeam-stats failedreplications=$Count"
 Write-Host $body
 
 $body="veeam-stats totalbackuptransfer=$totalxferBk"
+Write-Host $body
 
 foreach ($Repo in $RepoReport){
 $Name = "REPO " + $Repo."Repository Name" -replace '\s','_'
